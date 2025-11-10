be5012bf50306124943b30c1a86dd620039aeb25
addo561 <korlilarryaddo43@gmail.com>
2025-11-10 17:12:41 +0000

    tests done

--- tests.py
+++ tests.py
@@ -1,17 +1,22 @@
 #### SIMPLE TESTING 
 from models.backbone.efficient_b0 import  Mbconv_block
 import torch
+import unittest
 
-def testmb_con():
-    """Testing  MBconv  block. with stride 1 
-    """
-    input = torch.randn(2,32,64,64) #images
-    block = Mbconv_block(custom_stride=1,ch_in=32,ch_out=32,expansion=6)
 
-    out = block(input)
-    print(f'output shape {out.shape}')
-    print(f'input  shape {input.shape}')
-    print(f'{block.short_cut}') #should be True
+class testshort_cut(unittest.TestCase):
+    def testmb_con(self):
+        """Testing  MBconv  block. with stride 1  for skip  connection
+            Return:
+                True if shortcut 
+        """
+        input = torch.randn(2,32,64,64) #images
+        block = Mbconv_block(custom_stride=1,ch_in=32,ch_out=32,expansion=6)
 
-    print(out)
-testmb_con()    
+        out = block(input)
+        print(f'output shape {out.shape}')
+        print(f'input  shape {input.shape}')
+        self.assertEqual(block.short_cut,True) #check for skip  connection
+
+if __name__ == '__main__':
+    unittest.main()
