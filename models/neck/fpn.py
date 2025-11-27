9c41c628cd8801503d002b3725363bfdaafd8479
addo561 <korlilarryaddo43@gmail.com>
2025-11-27 00:12:15 +0000

    done  with test and  top down fpn

--- models/neck/fpn.py
+++ models/neck/fpn.py
@@ -1,30 +1,32 @@
+###FPN network  ,Top down
+
 from torch import nn
 import torch
 from models.backbone.efficient_b0 import  MultiScaleFeatureExtractor
-###FPN network  ,Top down
 
 #multiscale features
 features = MultiScaleFeatureExtractor()
 c2,c3,c4,c5 =  features.forward(torch.rand(1,3,224,224))
-print(c2.shape,c3.shape,c4.shape,c5.shape) #check input channels
+#print(c2.shape,c3.shape,c4.shape,c5.shape) #check input channels
 
 #from c5 -> c2 downsampled 
 class FPN(nn.Module):
     def __init__(self,output_channels=256):
         super(FPN,self).__init__()
-        self.c5_conv = nn.Conv2d(in_channels=input_channels,
+        self.extractor = MultiScaleFeatureExtractor()
+        self.c5_conv = nn.Conv2d(in_channels=320,
                                  out_channels=output_channels,
                                  kernel_size=1,
                                  stride=1)
-        self.c4_conv = nn.Conv2d(in_channels=input_channels,
+        self.c4_conv = nn.Conv2d(in_channels=40,
                                  out_channels=output_channels,
                                  kernel_size=1,
                                  stride=1)
-        self.c3_conv = nn.Conv2d(in_channels=input_channels,
+        self.c3_conv = nn.Conv2d(in_channels=24,
                                  out_channels=output_channels,
                                  kernel_size=1,
                                  stride=1)
-        self.c2_conv = nn.Conv2d(in_channels=input_channels,
+        self.c2_conv = nn.Conv2d(in_channels=16,
                                  out_channels=output_channels,
                                  kernel_size=1,
                                  stride=1)
@@ -32,6 +34,27 @@ class FPN(nn.Module):
         self.bn  = nn.BatchNorm2d(output_channels)
         self.silu = nn.SiLU(inplace=True)
     def forward(self,x):
-        c2,c3,c4,c5 = MultiScaleFeatureExtractor().forward(x)
-        block_5 = self.bn(self.silu(self.c5_conv(c5)))
-        print(block_5.shape)
+        c2,c3,c4,c5 = self.extractor(x)
+        block_5 = self.bn(self.silu(self.c5_conv(c5)))#(1,256,7,7)
+        block_4 = self.bn(self.silu(self.c4_conv(c4)))#(1,256,28,28)
+        block_3 = self.bn(self.silu(self.c3_conv(c3)))#(1,256,56,56)
+        block_2 = self.bn(self.silu(self.c2_conv(c2)))#(1,256,112,112)
+        P5 = block_5
+        #Upsample block 5 and add to  block4
+        upsample_scale_4 = nn.Upsample(scale_factor=4)
+        P5_up =  upsample_scale_4(P5)#(1,256,28,28)
+        P4 = P5_up + block_4 # shape (1,256,28,28)
+
+        #upsample block4_5  and add to block3
+        upsample_scale_2 = nn.Upsample(scale_factor=2)
+        P4_up = upsample_scale_2(P4) #(1,256,56,56)
+        P3 = P4_up + block_3#(1,256,56,56)
+
+        #upsample block3_4_5 and add to block2
+        P3_up = upsample_scale_2(P3)#(1,256,112,112)
+        P2 = P3_up + block_2#(1,256,112,112)
+
+        return P2,P3,P4,P5
+
+      
+        
--- tests.py
+++ tests.py
@@ -1,21 +1,17 @@
 #### SIMPLE TESTING 
-from models.backbone.efficient_b0 import EfficientNetB0_pretrained,efficientnet_b0
+from models.backbone.efficient_b0 import efficientnet_b0
 import torch
-import unittest
+import pytest
 from torchinfo import  summary
 
 
-class testshort_cut(unittest.TestCase):
-    def testmodel(self):
-        """checking efficientNet
-        """
-        input = torch.randn(2,3,224,224) #images
-        #model = EfficientNetB0_pretrained()
-        custom_model = efficientnet_b0()
+def testmodel():
+    """checking efficientNet
+    """
+    input = torch.randn(2,3,224,224) #images
+    custom_model = efficientnet_b0()
+    out = custom_model(input)
+    assert  out.size(1)==1000
 
-        #summary(model,input_data=input)
-        print('custom one')
-        summary(custom_model,input_data=input)
 
-if __name__ == '__main__':
-    unittest.main()
+
