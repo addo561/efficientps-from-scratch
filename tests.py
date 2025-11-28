f54741bba499eb91573dc90f88ec98d93c0d630c
addo561 <korlilarryaddo43@gmail.com>
2025-11-28 19:52:21 +0000

    some tests done

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 #### SIMPLE TESTING 
 from models.backbone.efficient_b0 import efficientnet_b0
+from models.neck.bifpn  import biFPN
 import torch
 import pytest
 from torchinfo import  summary
@@ -13,5 +14,9 @@ def testmodel():
     out = custom_model(input)
     assert  out.size(1)==1000
 
-
+def  bifpntest():
+    test_image =torch.rand(1,3,224,224)
+    p4,p6,p16,p32 =  biFPN().forward(test_image)
+    assert  p4.shape[2] == 112 
+    
 
