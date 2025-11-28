a8c7c7449a515e1344e4e428598f8ed648f16b78
addo561 <korlilarryaddo43@gmail.com>
2025-11-28 19:41:40 +0000

    bifpn and fpn changes maxpool for downsampling and moved all layers to __init__

--- models/neck/bifpn.py
+++ models/neck/bifpn.py
@@ -1,7 +1,7 @@
 from torch  import nn
 import torch
 import torch.nn.functional as F
-from torchvision import models
+from models.backbone.efficient_b0 import Depthwise_conv,pointwise_conv
 from models.neck.fpn import FPN
 
 ### BiFPNt -> p4,p8,p16,p32
@@ -9,32 +9,69 @@ class biFPN(FPN):
     def __init__(self):
         super(biFPN,self).__init__()
         #separable convolution
-        
-        
+        self.depthwise = Depthwise_conv(ch_in=256,
+                                        expansion=1,
+                                        custom_stride=1,
+                                        k=3
+                                        ) #All blocks have channels 256, same for pointwise
+        self.pointwise =  pointwise_conv(ch_in=256,
+                                         ch_out=256
+                                         )
+        self.downsample2 = nn.MaxPool2d(stride=2,kernel_size=2)
+        self.downsample4 = nn.MaxPool2d(stride=4,kernel_size=4)
 
     def forward(self,x):
         c2,c3,c4,c5 = self.extractor(x)
-        block_5 = self.bn(self.silu(self.c5_conv(c5)))#(1,256,7,7)
-        block_4 = self.bn(self.silu(self.c4_conv(c4)))#(1,256,28,28)
-        block_3 = self.bn(self.silu(self.c3_conv(c3)))#(1,256,56,56)
-        block_2 = self.bn(self.silu(self.c2_conv(c2)))#(1,256,112,112)
-        P2 = block_2
+        block_5 = self.L_relu(self.bn(self.silu(self.c5_conv(c5))))#(1,256,7,7)
+        block_4 = self.L_relu(self.bn(self.silu(self.c4_conv(c4))))#(1,256,28,28)
+        block_3 = self.L_relu(self.bn(self.silu(self.c3_conv(c3))))#(1,256,56,56)
+        block_2 = self.L_relu(self.bn(self.silu(self.c2_conv(c2))))#(1,256,112,112)
+        p2 = block_2
+
+        ### bottomUP approach
         #Downsample block 2 and add to  block3
-        downsample2 = nn.Upsample(scale_factor=0.5)
-        P2_down =  downsample2(P2)#(1,256,56,56)
-        P3 = P2_down + block_3 # shape (1,256,56,56)
+        P2_down =  self.downsample2(p2)#(1,256,56,56)
+        p3 = P2_down + block_3 # shape (1,256,56,56)
 
         #downsample P3 and add to block4
-        downsample2 = nn.Upsample(scale_factor=2)
-        P3_down = downsample2(P3) #(1,256,28,28)
-        P4 = P3_down + block_4#(1,256,28,28)
+        P3_down = self.downsample2(p3) #(1,256,28,28)
+        p4 = P3_down + block_4#(1,256,28,28)
 
         #downsample P4 and add to block5
-        P4_down = nn.Upsample(scale_factor=0.25)(P4)#(1,256,7,7)
-        P5 = P4_down + block_5#(1,256,7,7)
+        P4_down = self.downsample4(p4)#(1,256,7,7)
+        p5 = P4_down + block_5#(1,256,7,7)
         
         #call  forward method for FPN
         fpn2,fpn3,fpn4,fpn5 = super().forward(x)
+
+        # fpn and p blocks and  pass  through depthwise separable convolution
+        # P4
+        P4 = p2 + fpn2
+        P4 = self.depthwise(P4)
+        P4 = self.pointwise(P4)
+        P4 = self.L_relu(P4)
+
+        #P8
+        P8 = p3 + fpn3
+        P8 = self.depthwise(P8)
+        P8 = self.pointwise(P8)
+        P8 = self.L_relu(P8)
+
+        #P16
+        P16 = p4 + fpn4
+        P16 = self.depthwise(P16)
+        P16 = self.pointwise(P16)
+        P16 = self.L_relu(P16)
+
+        #P32
+        P32 = p5 + fpn5
+        P32 = self.depthwise(P32)
+        P32 = self.pointwise(P32)
+        P32 = self.L_relu(P32)
+
+        return  P4,P8,P16,P32
+
+
 
 
 
--- models/neck/fpn.py
+++ models/neck/fpn.py
@@ -34,6 +34,8 @@ class FPN(nn.Module):
         self.bn  = nn.BatchNorm2d(output_channels)
         self.silu = nn.SiLU(inplace=True)
         self.L_relu = nn.LeakyReLU(0.1,inplace=True)
+        self.upsample_scale_4  = nn.Upsample(scale_factor=4)
+        self.upsample_scale_2 = nn.Upsample(scale_factor=2)
     def forward(self,x):
         c2,c3,c4,c5 = self.extractor(x)
         block_5 = self.L_relu(self.bn(self.silu(self.c5_conv(c5))))#(1,256,7,7)
@@ -42,17 +44,15 @@ class FPN(nn.Module):
         block_2 = self.L_relu(self.bn(self.silu(self.c2_conv(c2))))#(1,256,112,112)
         P5 = block_5
         #Upsample block 5 and add to  block4
-        upsample_scale_4 = nn.Upsample(scale_factor=4)
-        P5_up =  upsample_scale_4(P5)#(1,256,28,28)
+        P5_up =  self.upsample_scale_4(P5)#(1,256,28,28)
         P4 = P5_up + block_4 # shape (1,256,28,28)
 
         #upsample P4  and add to block3
-        upsample_scale_2 = nn.Upsample(scale_factor=2)
-        P4_up = upsample_scale_2(P4) #(1,256,56,56)
+        P4_up = self.upsample_scale_2(P4) #(1,256,56,56)
         P3 = P4_up + block_3#(1,256,56,56)
 
         #upsample P3 and add to block2
-        P3_up = upsample_scale_2(P3)#(1,256,112,112)
+        P3_up = self.upsample_scale_2(P3)#(1,256,112,112)
         P2 = P3_up + block_2#(1,256,112,112)
 
         return P2,P3,P4,P5
