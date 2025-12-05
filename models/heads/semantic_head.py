578db4f24fbc74f79371627f07f7d850f8923985
addo561 <korlilarryaddo43@gmail.com>
2025-12-05 22:54:26 +0000

    done with semantic head tests

--- models/heads/semantic_head.py
+++ models/heads/semantic_head.py
@@ -18,9 +18,9 @@ def Separableconvolution(ch_in,ch_out,expansion,custom_stride,k, dilate:tuple=No
                 kernel_size=k,
                 groups=ch_in * expansion,
                 stride = custom_stride,
-                padding= padding if padding else k//2,
+                padding= padding if padding is not  None else k//2,
                 bias=False,
-                dilation= dilate if dilate else None
+                dilation= dilate if dilate is not None else 1
                 ),
             nn.Conv2d(
             in_channels=ch_in,
@@ -35,17 +35,25 @@ def Separableconvolution(ch_in,ch_out,expansion,custom_stride,k, dilate:tuple=No
     return block 
 
 
-class LSFE(nn.module):
-    '''Our Large Scale Feature Extractor(LSFE)
+class LSFE(nn.Module):
+    '''Our Large Scale Feature Extractor
+    '''
+    
+    def __init__(self,in_ch=256,out_ch = 128):
+        super().__init__()
+        '''Our Large Scale Feature Extractor(LSFE)
         Args:
             input channels,output_channels
         Returns:
             fetaure map of shape -> (128,H,W)    
-    '''
-    def __init__(self,in_ch=256,out_ch = 128):
-        super().__init__()
+        '''
         # 3 × 3 depthwise separable convolutions,with output filters of 128 
-        self.SeparableConv = Separableconvolution(ch_in=in_ch,
+        self.SeparableConv1= Separableconvolution(ch_in=in_ch,
+                                                  ch_out=out_ch,
+                                                  expansion=1,
+                                                  k=3,
+                                                  custom_stride=1)
+        self.SeparableConv2= Separableconvolution(ch_in=out_ch,
                                                   ch_out=out_ch,
                                                   expansion=1,
                                                   k=3,
@@ -58,31 +66,32 @@ class LSFE(nn.module):
         )
 
     def forward(self,x):
-        x = self.SeparableConv(x)
+        x = self.SeparableConv1(x)
         conv1 = self.norm_act(x)#(b,128,h,w)
-        x = self.SeparableConv(conv1)
+        x = self.SeparableConv2(conv1)
         conv2 = self.norm_act(x)  #(b,128,h,w)
         return conv2
     
    
 class DPC(nn.Module):
-    ''' Dense Prediction Cells (DPC) to help capture long range contexts.
-        DPC demonstrates a better performance with a minor increase in the number of parameters 
+    '''DPC demonstrates a better performance with a minor increase in the number of parameters 
+    '''
+    def __init__(self,channels=256):
+        super().__init__()
+        ''' Dense Prediction Cells (DPC) to help capture long range contexts.
         Args:
             channels
         Returns:
             128,h,w feature map    
 
-    '''
-    def __init__(self,channels=256):
-        super().__init__()
+        '''
         # Seperable  convolution with dilation rates
         # dilation rates (1,6)
         self.depthwise1_6 = Separableconvolution(ch_in=channels,
                                                 ch_out=channels,
                                                 expansion=1,
                                                 custom_stride=1,
-                                                d = (1,6),
+                                                dilate = (1,6),
                                                 k=3,
                                                 padding=(1,6))
         
@@ -91,7 +100,7 @@ class DPC(nn.Module):
                                                 ch_out=channels,
                                                 expansion=1,
                                                 custom_stride=1,
-                                                d = (1,1),
+                                                dilate = (1,1),
                                                 k=3,
                                                 padding=(1,1))
         #dilation (6,21)
@@ -99,7 +108,7 @@ class DPC(nn.Module):
                                                 ch_out=channels,
                                                 expansion=1,
                                                 custom_stride=1,
-                                                d = (6,21),
+                                                dilate = (6,21),
                                                 k=3,
                                                 padding=(6,21))
         #dilation (18,15)
@@ -107,7 +116,7 @@ class DPC(nn.Module):
                                                 ch_out=channels,
                                                 expansion=1,
                                                 custom_stride=1,
-                                                d = (18,15),
+                                                dilate = (18,15),
                                                 k=3,
                                                 padding=(18,15)
                                                 )
@@ -115,7 +124,7 @@ class DPC(nn.Module):
                                                 ch_out=channels,
                                                 expansion=1,
                                                 custom_stride=1,
-                                                d = (6,3),
+                                                dilate = (6,3),
                                                 k=3,
                                                 padding=(6,3)
                                                 )
@@ -124,7 +133,7 @@ class DPC(nn.Module):
             nn.SiLU(inplace=True),
             nn.LeakyReLU(0.1,inplace=True)
         )
-        self.conv = nn.Conv2d(in_channels=256,out_channels=128,kernel_size=1,stride=1)
+        self.conv = nn.Conv2d(in_channels=1280,out_channels=128,kernel_size=1,stride=1)
     def forward(self,x):
         block1_6 = self.norm_act(self.depthwise1_6(x)) 
         block1_1 = self.norm_act(self.depthwise1_1(block1_6)) 
@@ -137,16 +146,24 @@ class DPC(nn.Module):
         
 
 class MC(nn.Module):
-    '''Mismatch Correction Module (MC) that correlates the small-scale features with respect to large-scale features.
+    '''mitigate the mismatch between large-scale and small-scale features
+    '''
+
+    def __init__(self,in_ch = 256,out_ch=128):
+        super().__init__()
+        '''Mismatch Correction Module (MC) that correlates the small-scale features with respect to large-scale features.
         Args:
             input channels,output_channels
         Returns:
             fetaure map of shape -> (128,H,W) upsampled by 2 
-    '''
-    def __init__(self,in_ch = 256,out_ch=128):
-        super().__init__()
+        '''
         #3 × 3 depthwise separable convolutions,with output filters of 128 
-        self.SeparableConv = Separableconvolution(ch_in=in_ch,
+        self.SeparableConv1 = Separableconvolution(ch_in=in_ch,
+                                                  ch_out=out_ch,
+                                                  expansion=1,
+                                                  k=3,
+                                                  custom_stride=1)
+        self.SeparableConv2 = Separableconvolution(ch_in=out_ch,
                                                   ch_out=out_ch,
                                                   expansion=1,
                                                   k=3,
@@ -159,9 +176,9 @@ class MC(nn.Module):
         )
         self.upsample =  nn.Upsample(scale_factor=2)
     def forward(self,x):
-        x = self.SeparableConv(x)
+        x = self.SeparableConv1(x)
         conv1 = self.norm_act(x)#(b,128,h,w)
-        x = self.SeparableConv(conv1)
+        x = self.SeparableConv2(conv1)
         conv2 = self.norm_act(x)  #(b,128,h,w)
         return self.upsample(conv2)
 
---
+++ tests/test_semanticHead.py
@@ -0,0 +1,22 @@
+import pytest
+import torch
+from models.heads.semantic_head import LSFE,DPC,MC
+
+p4 = torch.rand(1,256,256,512)
+p8 = torch.rand(1,256,128,256)
+p16 = torch.rand(1,256,64,128)
+p32 = torch.rand(1,256,32,64)
+test_case = [(p4,(1,128,256,512)),(p32,(1,128,32,64)),(p16,(1,128,64,128)),(p8,(1,128,128,256))]
+
+class TestSemanticHead:
+    @pytest.mark.parametrize('test_input,expected',[test_case[0],test_case[-1]])
+    def test_lsfe(self,test_input,expected):
+        module = LSFE()
+        x =  module(test_input)
+        assert x.shape== expected ,f'Test failed for input shape {x.shape} expected {expected}'    
+
+    @pytest.mark.parametrize('test_input,expected',[test_case[1],test_case[2]])  
+    def test_dpc(self,test_input,expected):
+        module = DPC()
+        x =  module(test_input) 
+        assert x.shape== expected,f'Test failed for input shape {x.shape} expected {expected}'    
