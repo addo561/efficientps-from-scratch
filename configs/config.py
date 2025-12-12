256eb46586db0d22df71e97c161c0129d23b8634
addo561 <korlilarryaddo43@gmail.com>
2025-12-12 15:10:12 +0000

    config

---
+++ configs/config.py
@@ -0,0 +1,33 @@
+
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+
+def  setup_efficient_config_instance_head():
+    cfg =  get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file('COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml'))
+    cfg.MODEL.ROI_HEADS.IN_FEATURES = ["p2", "p3", "p4", "p5"]
+    cfg.MODEL.RPN.IN_FEATURES = ["p2", "p3", "p4", "p5"]
+    cfg.MODEL.PROPOSAL_GENERATOR.NAME = "RPN"
+    # EfficientPS typically uses standard anchor sizes/ratios
+    cfg.MODEL.ANCHOR_GENERATOR.SIZES = [[32], [64], [128], [256], [512]]
+    cfg.MODEL.ANCHOR_GENERATOR.ASPECT_RATIOS = [[0.5, 1.0, 2.0]]
+
+    # ----------------------------------------------------------------
+    #  ROI HEADS (Box, Class, Mask)
+    # ----------------------------------------------------------------
+    cfg.MODEL.ROI_HEADS.NAME = "EfficientPSROIHeads"
+    
+    # BOX HEAD (Class + Box): 
+    # Paper uses standard 2 FC layers.
+    cfg.MODEL.ROI_BOX_HEAD.NAME = "FastRCNNConvFCHead"
+    cfg.MODEL.ROI_BOX_HEAD.NUM_FC = 2
+    cfg.MODEL.ROI_BOX_HEAD.POOLER_RESOLUTION = 7
+    
+    # MASK HEAD: 
+    # custom one
+    cfg.MODEL.ROI_MASK_HEAD.NAME = "EfficientPSMaskHead"
+    cfg.MODEL.ROI_MASK_HEAD.NUM_CONV = 4
+    cfg.MODEL.ROI_MASK_HEAD.CONV_DIM = 256
+    cfg.MODEL.ROI_MASK_HEAD.POOLER_RESOLUTION = 14
+
+    return cfg
