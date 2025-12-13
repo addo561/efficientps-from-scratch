97a29abc02f5b4703c50b35485a07743011312d7
addo561 <korlilarryaddo43@gmail.com>
2025-12-13 21:11:43 +0000

    dataset  registered

--- data/RegisterDS.py
+++ data/RegisterDS.py
@@ -1,4 +1,26 @@
 from detectron2.data.datasets import register_coco_instances
-trainR = register_coco_instances("YourTrainDatasetName", {},"path to train.json", "path to train image folder")
-testR = register_coco_instances("YourTestDatasetName", {}, "path to test.json", "path to test image folder")
+from  detectron2.data import MetadataCatalog,DatasetCatalog
+import  random
+import cv2
+import os
+from pathlib import Path
+from detectron2.utils.visualizer import Visualizer
 
+for name in ["trainDS", "testDs"]:
+    if name in DatasetCatalog.list():
+        DatasetCatalog.remove(name)
+    if name in MetadataCatalog.list():
+        MetadataCatalog.remove(name)
+
+
+base = Path(__file__).resolve().parent
+trainR = register_coco_instances("trainDS", {},f"{base}/Panoptic-Segmentation/train/_annotations.coco.json", f"{base}/Panoptic-Segmentation/train")
+testR = register_coco_instances("testDs", {}, f"{base}/Panoptic-Segmentation/test/_annotations.coco.json", f"{base}/Panoptic-Segmentation/test")
+train_metadata = MetadataCatalog.get("trainDS")
+data_dicts = DatasetCatalog.get("trainDS")
+for d in random.sample(data_dicts,4 ):
+    img = cv2.imread(d["file_name"])
+    visualizer = Visualizer(img[:, :, ::-1], metadata=train_metadata, scale=0.5)
+    out = visualizer.draw_dataset_dict(d)
+    cv2.imshow('Image',out.get_image()[:, :, ::-1])
+    cv2.waitKey(0)
