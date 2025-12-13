5498dd63999e29fc7c8095040f76eda5d9ff7931
addo561 <korlilarryaddo43@gmail.com>
2025-12-13 23:25:31 +0000

    done

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from configs.config import setup_efficient_config
 def main(args):
     # 1. Setup Config
     # We pass the class weights here if you have them calculated, else []
-    cfg = setup_efficient_config(num_classes=80, class_weights_list=[]) 
+    cfg = setup_efficient_config(num_classes=80) 
     
     # Override defaults with command line args if needed (e.g. changing learning rate)
     cfg.merge_from_list(args.opts)
